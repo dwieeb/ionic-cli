f4934e2abf1dbb68b9cc791e559d015d8c2b7327
Daniel Imhoff <dwieeb@gmail.com>
2017-11-29 19:14:13 -0600

    wip

--- packages/ionic/src/commands/daemon.ts
+++ packages/ionic/src/commands/daemon.ts
@@ -131,7 +131,7 @@ export class DaemonCommand extends Command {
       await this.env.daemon.save();
     };
 
-    registerShutdownFunction(this.env, () => {
+    registerShutdownFunction(() => {
       const fs = require('fs');
 
       try {
